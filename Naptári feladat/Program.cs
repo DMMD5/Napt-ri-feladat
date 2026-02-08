96b99cc731fdef99654b50f7eb2d7a0c86e06744
DMMD5 <b3romdor@vasvari.org>
2026-02-08 22:08:39 +0100

    struktura

--- Naptári feladat/Program.cs
+++ Naptári feladat/Program.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+struct Esemeny
+{
+    public string Tulajdonos;
+    public DateTime Idopont;
+    public int Idotartam;
+}
 
 namespace Naptári_feladat
 {
     internal class Program
     {
+        static List<Esemeny> esemenyek = new List<Esemeny>();
+        static Random rnd = new Random();
+        const int EV = 2028;
+        const int HONAP = 2;
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("\nnaptár");
+                Console.WriteLine("1 - Megjelenítése");
+                Console.WriteLine("2 - Új esemény");
+                Console.WriteLine("3 - Legközelebbi esemény");
+                Console.WriteLine("4 - Kilépés");
+                Console.Write("Választás: ");
+
+                string menu = Console.ReadLine();
+
+                switch (menu)
+                {
+                    case "1":
+                        return;
+                    case "2":
+                        return;
+                    case "3":
+                        return;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Érvénytelen választás!");
+                        break;
+                }
+            }
+
         }
     }
 }
